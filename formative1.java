b95ad3725f4a3462da66462a20c4f4c121fc6f2d
Timon <19689305+timonvdvelde@users.noreply.github.com>
2020-01-22 08:27:32 +0100

    init

---
+++ formative1.java
@@ -0,0 +1,14 @@
+/**
+ * Please put your student ID here
+ * ID: 
+ */
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+  }
+}
+
